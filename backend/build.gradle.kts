defee16d230dd820c87fd6b865698eb2faea0312
Marco Garofalo <mgarofalo@vmware.com>
2023-09-28 16:23:19 +0100

    Ensure the backend can serve the frontend app

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -25,3 +25,51 @@ dependencies {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+tasks.register<Exec>("installFrontend") {
+	inputs.file("../frontend/package.json")
+	inputs.file("../frontend/package-lock.json")
+	outputs.dir("../frontend/node_modules")
+	environment("NODE_ENV", "development") // Needed so that "vite" is installed (e.g. we are able to build!)
+	commandLine("env")
+	commandLine("npm", "--prefix", "../frontend", "install", "../frontend")
+}
+
+tasks.register<Exec>("buildFrontend") {
+	dependsOn("installFrontend")
+	inputs.dir("../frontend")
+	outputs.dir("../frontend/dist")
+	environment("NODE_ENV", "production")
+	commandLine("npm", "--prefix", "../frontend", "run", "build")
+}
+
+tasks.register<Sync>("copyFrontend") {
+	dependsOn("buildFrontend")
+	from("../frontend/dist")
+	into("$buildDir/resources/main/static")
+	doLast {
+		println("copied built frontend to static resources")
+	}
+}
+
+tasks.register<Delete>("cleanFrontend") {
+	delete("../frontend/dist")
+	delete("../src/main/resources/static")
+}
+
+// Needed for Gradle v8
+tasks.resolveMainClassName {
+	dependsOn("copyFrontend")
+}
+
+tasks.jar {
+	dependsOn("copyFrontend")
+}
+
+tasks.bootRun {
+	dependsOn("copyFrontend")
+}
+
+tasks.clean {
+	dependsOn("cleanFrontend")
+}
